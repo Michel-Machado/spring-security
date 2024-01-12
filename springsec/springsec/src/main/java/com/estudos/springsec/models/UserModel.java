ee70fd7a6e256f9e7ca61656fb407d0a56ed206c
Michel Machado <michelmachado125@gmail.com>
2024-01-12 11:29:10 -0300

    Add dependências Spring Security Entidade User e implementação da interface UserDetails

--- springsec/springsec/src/main/java/com/estudos/springsec/models/UserModel.java
+++ springsec/springsec/src/main/java/com/estudos/springsec/models/UserModel.java
@@ -3,13 +3,59 @@ package com.estudos.springsec.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "tb_user")
-public class UserModel {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel implements UserDetails {
+
     @Id
+    private String id;
     private String login;
     private String senha;
     private String role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
